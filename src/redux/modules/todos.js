97a2294d6a28096a410fb46bcf3bc59d43d37603
sejoon Lee <km19105@naver.com>
2023-02-16 13:21:09 +0900

    no message

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -59,8 +59,7 @@ const todos = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
       return {
-        ...state,
-        todos: [action.payload],
+        todos: [...state, action.payload],
       };
 
     case TOGGLE_STATUS_TODO:
